3c2ff54aa349162f9465a4d222a50c0c71614742
dobata <dkralev90@gmail.com>
2014-09-08 01:38:32 +0300

    Customizing the naviagion module demo code

--- jqmliketransitions/navigation.js
+++ jqmliketransitions/navigation.js
@@ -1,105 +1,107 @@
 var navigation = (function ( $ ){
-	'use strict';
-	var navModule = {},
-		pageTransitions = {
-			'slideRightToLeft': {
-				'outDirection': 'page-move-to-left',
-				'inDirection': 'page-move-from-right'
-			},
-			'slideLeftToRight': {
-				'outDirection': 'page-move-to-right',
-				'inDirection': 'page-move-from-left'
-			}
-		},
-		animEndEventNames = {
-			'WebkitAnimation' : 'webkitAnimationEnd',
-			'OAnimation' : 'oAnimationEnd',
-			'msAnimation' : 'MSAnimationEnd',
-			'animation' : 'animationend'
-		},
-		// general css classes for the animation
-		currentPageClass = 'current-page',
-		transitionPageClass = 'transition-page';
+    'use strict';
+    var navModule = {},
+        viewTransitions = {
+            'sliderighttoleft': {
+                'outDirection': 'view-move-to-left',
+                'inDirection': 'view-move-from-right'
+            },
+            'slidelefttoright': {
+                'outDirection': 'view-move-to-right',
+                'inDirection': 'view-move-from-left'
+            }
+        },
+        animEndEventNames = {
+            'WebkitAnimation' : 'webkitAnimationEnd',
+            'OAnimation' : 'oAnimationEnd',
+            'msAnimation' : 'MSAnimationEnd',
+            'animation' : 'animationend'
+        },
 
-	navModule.init = function () {
-		var _this = this;
-		$( document ).on( 'click', '.page-link' , function( e ) {
-			e.preventDefault();
-			e.stopPropagation();
-
-			var $this = $( this ),
-				$currentPage = $this.closest( '.page' ),
-				destinationPageId = $this.data( 'pageLinkDest' ),
-				$destinationPage = $( '#' + destinationPageId ),
-				pageTransition = $this.data( 'pageTransition' );
-
-			var	isPageChangeStartEventSuccessful = _this.triggerEvent( 'pagechangestart', {
-					$currentPage: $currentPage,
-					$destinationPage: $destinationPage,
-					$link: $this
-				});
+        // general css classes for the animation
+        currentViewClass = 'current-view',
+        transitionViewClass = 'transition-view';
 
 
-			// If the event returns false it will wait for a 'pagechangecontinue' event to trigger the page change
-			if( isPageChangeStartEventSuccessful ) {
-				_this.changePage({
-					$currentPage: $currentPage,
-					$destinationPage: $destinationPage,
-					pageTransition: pageTransition
-				});
-			}
-			else {
-				_this.addEventListener( 'pagechangecontinue', function () {
-					_this.changePage({
-						$currentPage: $currentPage,
-						$destinationPage: $destinationPage,
-						pageTransition: pageTransition
-					});
-				});
-			}
-		});
-	}
+    /**
+     * Performs animation-classes changing.
+     * @param  {jQueryObject} options.$currentView      represents the current view that the user sees
+     * @param  {jQueryObject} options.$destinationView      represents the view the user is going to transition to
+     * @param  {String} options.viewTransition      defines the transition animation type
+     */
+    function changeView ( options ) {
+        // NOTE: here we explicitly choose the Webkit vendor prefix
+        // next line should detect browser type and is left like that for the sake of simplicity.
 
-	/**
-	 * Changes the page from the current to the destination one with specified transition
-	 * @param  {jQuery} options.$currentPage  the current page
-	 * @param  {jQuery} options.$destinationPage  the destination page
-	 * @param  {String} options.pageTransition  the transition of the page change
-	 */
-	navModule.changePage = function ( options ) {
-		// animation classes
-		var animationEndEvent = animEndEventNames[ 'WebkitAnimation' ],
-			_this = this,
-			outClass,
-			inClass;
+        // animation classes
+        var animationEndEvent = animEndEventNames[ 'WebkitAnimation' ],
+            _this = this,
+            outClass,
+            inClass;
 
-		if( options.pageTransition === undefined ) {
-			// default transition
-			options.pageTransition = 'slideRightToLeft';
-		}
+        if( options.viewTransition === undefined ) {
+            // default transition
+            options.viewTransition = 'sliderighttoleft';
+        }
 
-		outClass = transitionPageClass + ' ' + pageTransitions[ options.pageTransition ].outDirection;
-		inClass = transitionPageClass + ' ' + pageTransitions[ options.pageTransition ].inDirection;
+        outClass = transitionViewClass + ' ' + viewTransitions[ options.viewTransition ].outDirection;
+        inClass = transitionViewClass + ' ' + viewTransitions[ options.viewTransition ].inDirection;
 
-		// _this.triggerEvent( 'navigationstart' );
+        options.$currentView.removeClass( currentViewClass )
+            .addClass( outClass );
 
-		options.$currentPage.removeClass( currentPageClass )
-			.addClass( outClass );
+        options.$destinationView.addClass( currentViewClass )
+                .addClass( inClass );
 
-		options.$destinationPage.addClass( currentPageClass )
-				.addClass( inClass );
+        options.$currentView.on( animationEndEvent, function() {
+            options.$currentView.removeClass( outClass );
+        });
 
-		options.$currentPage.on( animationEndEvent, function() {
-			options.$currentPage.removeClass( outClass );
-		});
+        options.$destinationView.on( animationEndEvent, function() {
+            options.$destinationView.removeClass( inClass );
+        });
+    }
 
-		options.$destinationPage.on( animationEndEvent, function() {
-			options.$destinationPage.removeClass( inClass );
-			_this.triggerEvent( 'pagechangeend' );
-		});
-	}
+    /**
+     * Manually trigger view-change
+     * @param  {jQueryObject} options.viewDestinationId     the Id of the destination view we want to transition to
+     * @param  {jQueryObject} options.viewTransition    the animation transition type
+     * @return {[type]}         [description]
+     */
+    navModule.triggerViewChange = function( options ) {
 
-	eventy.eventEnable( navModule );
+    }
 
-	return navModule;
+    /**
+     * Binds view-changing animations on links click event.
+     * The eventListener is binded to the document, and that
+     * ensures that if we dynamically add markup to our views, the links
+     * will work.
+     */
+    navModule.init = function () {
+        var _this = this;
+        $( document ).on( 'click', '.view-link' , function( e ) {
+            e.preventDefault();
+            e.stopPropagation();
+
+            var $this = $( this ),
+                $currentView = $this.closest( '.view' ),
+                destinationViewId = $this.data( 'viewLinkDest' ),
+                $destinationView = $( '#' + destinationViewId ),
+                viewTransition = $this.data( 'viewTransition' );
+
+
+            changeView({
+                $currentView: $currentView,
+                $destinationView: $destinationView,
+                viewTransition: viewTransition
+            });
+        });
+    }
+
+    return navModule;
 })( jQuery );
+
+
+navigation.init();
+
